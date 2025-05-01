4536d889a818bba655730501465cd503ec865336
adam-pw <adampithewan@gmail.com>
2025-05-01 23:49:28 +0530

    init go gin gorm project

--- database/database.go
+++ database/database.go
@@ -3,6 +3,7 @@ package database
 import (
     "fmt"
     "log"
+    "os"
     "gorm.io/driver/postgres"
     "gorm.io/gorm"
     "github.com/adam-pw/models"
@@ -11,7 +12,14 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-    dsn := "host=localhost user=adampithenwala password=12345678 dbname=go_gin_db port=5432 sslmode=disable"
+    dsn := fmt.Sprintf(
+        "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+        os.Getenv("DB_HOST"),
+        os.Getenv("DB_USER"),
+        os.Getenv("DB_PASSWORD"),
+        os.Getenv("DB_NAME"),
+        os.Getenv("DB_PORT"),
+    )
     var err error
     DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
     if err != nil {
--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+    "log"
+    "github.com/joho/godotenv"
     "github.com/gin-gonic/gin"
     "github.com/adam-pw/database"
     "github.com/adam-pw/routes"
 )
 
 func main() {
+    err := godotenv.Load() // load .env file
+    if err != nil {
+        log.Fatal("Error loading .env file") // log error if .env file is not found
+    }
     r := gin.Default()
     database.Connect()      // connect to DB
     routes.RegisterRoutes(r) // register routes
